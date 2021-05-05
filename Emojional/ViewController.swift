118cafa767b844b85624d94fe836373474f5dc9b
Fiona Oh <fionaoh2002@gmail.com>
2021-05-05 02:05:34 -0700

    fixed some code formatting

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -10,9 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     let emojis = ["🥲": "single tear", "😖": "embarrassed", "😴": "sleepy", "🥰": "love"]
-
     let customMessages1 = ["single tear" : ["it do be like that sometimes", "it's ok!!", "you got this!"], "embarrassed" : ["no one remembers!", "it happens to everyone", "it's in the past! look to the future"], "sleepy" : ["time for a nap!", "let's take a break tonight", "sleep in tomorrow morning!"], "love" : ["happy day!", "you are wonderful", "you are loved"]]
-    
     let customMessages2 = ["single tear" : ["tough times never last but tough people do", "and still, I rise", "embrace the glorious mess that you are"], "embarrassed" : ["I have nothing to lose but something to gain", "turn your wounds into wisdom", "I will remember and recover, not forgive and forget"], "sleepy" : ["reality is wrong, dreams are for real", "determine your priorities and focus on them", "dream as if you'll live forever, live as if you'll die today"], "love" : ["love for all, hatred for none", "let the beauty of what you love be what you do", "a happy soul is the best shield for a cruel world"]]
     
     var list = true
@@ -26,7 +24,6 @@ class ViewController: UIViewController {
         //print out a message!
         
         let selectedEmotion = sender.titleLabel?.text
-  
         let options : [String]
         
         if list {
@@ -36,7 +33,6 @@ class ViewController: UIViewController {
         }
         
         let emojiMessage = options[Int.random(in: 0..<3)]
-        
         let alertController = UIAlertController(title: emojis[selectedEmotion!]!, message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         
         func handler(alert: UIAlertAction!) {
@@ -44,7 +40,6 @@ class ViewController: UIViewController {
         }
         
         alertController.addAction(UIAlertAction(title: "Thank you!", style: UIAlertAction.Style.default, handler: nil))
-        
         alertController.addAction(UIAlertAction(title: "Unhelpful..", style: UIAlertAction.Style.default, handler: handler))
         
         present(alertController, animated: true, completion: nil)
